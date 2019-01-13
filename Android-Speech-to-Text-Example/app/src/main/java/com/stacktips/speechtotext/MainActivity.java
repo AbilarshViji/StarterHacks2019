c9ff98ba388c75585f9a4580279ba71ef7b19763
AbilarshViji <AbilarshViji@users.noreply.github.com>
2019-01-13 14:40:47 -0500

    get rid of unnecessary code

--- Android-Speech-to-Text-Example/app/src/main/java/com/stacktips/speechtotext/MainActivity.java
+++ Android-Speech-to-Text-Example/app/src/main/java/com/stacktips/speechtotext/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     private RecognizerIntent myText;
     private boolean found = false;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void speechInput(){
+    private void speechInput() {
 
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
@@ -56,18 +55,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),
                     getString(R.string.speech_not_supported),
                     Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void startVoiceInput() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
-
         }
     }
 
@@ -104,13 +91,6 @@ public class MainActivity extends AppCompatActivity {
                             dialPhoneNumber(phoneNumberString);
                         }
                     }
-                    /**
-                    if (result.contains("pineapple")){
-                        found = true;
-                        dialPhoneNumber("1234567890");
-                     }
-                     */
-//                  mVoiceInputTv.setText(result.get(0));
                 }
                 break;
             }
