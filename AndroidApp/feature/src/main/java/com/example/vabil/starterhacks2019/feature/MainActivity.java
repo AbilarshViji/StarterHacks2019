3674fe3da9bdd79773c8bd62137f78cb26bdceda
ChenyanWang <16136996+ChenyanWang@users.noreply.github.com>
2019-01-13 09:30:34 -0500

    Changed to dial function

--- AndroidApp/feature/src/main/java/com/example/vabil/starterhacks2019/feature/MainActivity.java
+++ AndroidApp/feature/src/main/java/com/example/vabil/starterhacks2019/feature/MainActivity.java
@@ -12,35 +12,21 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private static final int REQUEST_PHONE_CALL = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.callButton);
-        //Request permissions
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:0377778888"));
-
-
-                //Checks if permission has been granted
-                if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
-                    return;
-                }
-                System.out.print("inclick");
-
-                startActivity(callIntent);
+        findViewById(R.id.callButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                dialPhoneNumber("1234567890");
             }
         });
+    }
 
+    private void dialPhoneNumber(final String phoneNumber){
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
     }
 
     /*
